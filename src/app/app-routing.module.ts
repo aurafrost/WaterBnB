38fee45e116f86edf681f28ec004a479b48a5293
jimmytran <jt963369@gmail.com>
2019-02-11 09:31:15 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PoolsComponent } from './pools/pools.component';
+import { ReviewComponent } from './review/review.component';
+import { SigninComponent } from './signin/signin.component';
+import { HostComponent } from './host/host.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '/', component: SearchComponent},
+  {path: '/pools/:id', component: PoolsComponent},
+  {path: '/review', component: ReviewComponent},
+  {path: '/guest/signin', component: SigninComponent},
+  {path: '/host', component: HostComponent},
+  {path: '/dashboard', component: DashboardComponent},
+  {path: '**', component: AppComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { PoolsComponent } from './pools/pools.component';
+import { RestService } from './services/rest.service';
+import { ReviewComponent } from './review/review.component';
+import { SigninComponent } from './signin/signin.component';
+import { HostComponent } from './host/host.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    PoolsComponent,
+    ReviewComponent,
+    SigninComponent,
+    HostComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [RestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
