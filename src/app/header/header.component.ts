407354129b2f7222450ac245df46e4dc760edfbc
jimmytran <jt963369@gmail.com>
2019-02-12 14:39:05 -0500

    header functionally complete

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../services/rest.service';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
 
 @Component({
   selector: 'header',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  user:User;
+  constructor(private service:RestService,
+    private router:Router) { }
 
   ngOnInit() {
+    
   }
 
+  checkStatus(){
+    this.user=this.service.getCurrentUser();
+    if(this.user==null||this.user.type=="Guest"){
+      this.router.navigate(["/"]);
+    }
+    else{
+      this.router.navigate(["host"]);
+    }
+  }
+
+  checkStatusLogin(){
+    this.user=this.service.getCurrentUser();
+    if(this.user==null){
+      this.router.navigate(["/guest/signin"]);
+    }
+    else if(this.user.type=="Guest"){
+
+    }
+    else{
+      
+    }
+  }
+
+  logout(){
+    this.service.setCurrentUser(null);
+    this.router.navigate(['/']);
+  }
 }
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -9,50 +9,54 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
-  user:User=new User();
-  type:string;
-  fName:string;
-  lName:string;
-  email:string;
-  password:string;
+  user: User = new User();
+  type: string;
+  fName: string;
+  lName: string;
+  email: string;
+  password: string;
 
-  signinemail:string;
-  signinpassword:string;
-  errormsg:string;
-  constructor(private service:RestService,
-    private router:Router) { }
+  signinemail: string;
+  signinpassword: string;
+  errormsg: string;
+  constructor(private service: RestService,
+    private router: Router) { }
 
   ngOnInit() {
   }
-  
-  register(type,fName,lName,email,password){
-    this.user.type=type;
-    this.user.fName=fName;
-    this.user.lName=lName;
-    this.user.email=email;
-    this.user.password=password;
-    this.service.registerUser(this.user).subscribe(data=>{});
+
+  register(type, fName, lName, email, password) {
+    this.user.type = type;
+    this.user.fName = fName;
+    this.user.lName = lName;
+    this.user.email = email;
+    this.user.password = password;
+    this.service.registerUser(this.user).subscribe(data => { });
     console.log("registering...");
     this.router.navigate(["/"]);
   }
 
-  login(signinemail,signinpassword){
-    this.service.getUserByEmail(signinemail).subscribe(data=>{
-      console.log("Returning email: "+data.email);
-      if(data==null){
-        this.errormsg="Unregistered email.";
+  login(signinemail, signinpassword) {
+    this.service.getUserByEmail(signinemail).subscribe(data => {
+      console.log("Returning email: " + data.email);
+      if (data == null) {
+        this.errormsg = "Unregistered email.";
       }
-      else if(data.password!=signinpassword){
-        this.errormsg="Wrong password";
+      else if (data.password != signinpassword) {
+        this.errormsg = "Wrong password";
       }
-      else{
+      else {
         //hold user in session
         this.service.setCurrentUser(data);
-        console.log("Signed in as "+data.email);
+        console.log("Signed in as " + data.email);
         //navigate to dashboard if host
-        this.router.navigate(["host"]);
+        if (data.type=="Host") {
+          this.router.navigate(["host"]);
+        }
         //navigate to home if guest
-        this.router.navigate(["/"]);
+        else {
+          this.router.navigate(["/"]);
+        }
       }
     });
   }
